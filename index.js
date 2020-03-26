edbad7732b0616e401a069b09db50ea3a61b0661
Todd Christensen <toddchristensen@Todds-MacBook-Pro.local>
2020-03-27 03:47:53 +0800

    refactoring getTweets into function

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 var Twit = require("twit");
 require("dotenv").config();
 
-var T = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true // optional - requires SSL certificates to be valid.
-}); 
+function getTweets(opts) {
 
-const options = {
-  screen_name: "PeterSchiff",
-  trim_user: true,
-  count: 1000,
-  exclude_replies: true,
-  include_rts: false
-};
+    const {screen_name, keywords} = opts;
 
-T.get("statuses/user_timeline", options, function(err, data) {
+    var T = new Twit({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+      timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+      strictSSL: true // optional - requires SSL certificates to be valid.
+    });
 
-    const keywords = ['Bitcoin', 'bitcoin', 'btc', 'btc'];
-  
-    keywords.forEach(keyword => {
+    let tweets = [];
+
+    const options = {
+      screen_name,
+      trim_user: true,
+      count: 1000,
+      exclude_replies: true,
+      include_rts: false,
+      keywords,
+    };
+
+    T.get("statuses/user_timeline", options, function(err, data) {
+
+        
+
+      options.keywords.forEach(keyword => {
         data.filter(tweet => {
-          if (tweet.text.includes(keyword)) console.log(tweet.text);
+          if (tweet.text.includes(keyword)) tweets += tweet.text;
         });
-    })
+      });
 
+      console.log(tweets);
+    });
+}
+
+
+getTweets({
+  screen_name: "PeterSchiff",
+  keywords: ["Bitcoin", "bitcoin", "btc", "btc"]
 });
